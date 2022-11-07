7d221e2d2d7faa9ac17d675145c14edcb404c521
mehnazimu <mehnaz.imu@gmail.com>
2022-11-07 08:11:47 +0600

    added

--- index.js
+++ index.js
@@ -59,6 +59,13 @@ async function run() {
             res.send(result);
             console.log(result);
         })
+
+        app.delete('/orders/:id', async (req,res)=>{
+            const id= req.params.id;
+            const query ={_id: ObjectId(id)};
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally {
 
